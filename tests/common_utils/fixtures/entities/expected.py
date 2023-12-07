89a3990c9923ddc1d8453bbe33ddf70dd1276ce3
Harold Cohen <harold.cohen@thetribe.io>
2023-12-07 17:35:33 +0100

    test: remove use_dedicated_properties fixture

--- tests/common_utils/fixtures/entities/expected.py
+++ tests/common_utils/fixtures/entities/expected.py
@@ -6,7 +6,6 @@ __all__ = [
     "expected_entity",
     "expected_entities",
     "use_properties",
-    "use_dedicated_properties",
 ]
 
 
@@ -34,22 +33,12 @@ def expected_entity(
 def expected_entities(
         request,
         expected_unit_of_work_memento,
-        use_properties,
-        use_dedicated_properties
 ):  # pylint: disable=redefined-outer-name
     """Returns multiple entity instances to be used for assertion (and action as well)."""
     if hasattr(request, "param"):
-        properties_idx = 0
         for entity in request.param:
-            if use_properties:
-                for prop, value in use_properties.items():
-                    setattr(entity, prop, value)
-            if use_dedicated_properties:
-                for prop, value in use_dedicated_properties[properties_idx].items():
-                    setattr(entity, prop, value)
             table = getattr(expected_unit_of_work_memento, entity.__tablename__)
             table[entity.id] = entity
-            properties_idx += 1
 
         return request.param
 
@@ -61,10 +50,3 @@ def use_properties(request):  # pylint: disable=redefined-outer-name
     if hasattr(request, "param"):
         return request.param
     return {}
-
-
-@pytest.fixture()
-def use_dedicated_properties(request):
-    if hasattr(request, "param"):
-        return request.param
-    return []
