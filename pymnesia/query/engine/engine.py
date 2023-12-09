6c1e8e960465ed1a886b46efd2caa0a655091fe3
Harold Cohen <harold.cohen@thetribe.io>
2023-12-09 16:25:46 +0100

    refactor(query): implement registry as attr in engine metacls

--- pymnesia/query/engine/engine.py
+++ pymnesia/query/engine/engine.py
@@ -1,17 +1,18 @@
 """Provides with QueryEngine.
 """
-from pymnesia.query.engine.meta import QueryEngineType
+from pymnesia.query.engine.meta import query_engine_metaclass
+from pymnesia.entities.registry import registry  # pylint: disable=unused-import
 
 
-class QueryEngine(metaclass=QueryEngineType):
+class QueryEngine(metaclass=query_engine_metaclass(registry_=registry)):
     """
-    QueryEngine class.
+    Default QueryEngine class that uses a default global registry.
 
     Allows to query a unit of work tables.
     """
 
     def __init__(
             self,
-            unit_of_work
+            unit_of_work,
     ):
         self.unit_of_work = unit_of_work
--- pymnesia/query/engine/meta.py
+++ pymnesia/query/engine/meta.py
@@ -1,7 +1,13 @@
 """Provides with a metaclass for QueryEngine.
 """
+from pymnesia.query.engine.base import QueryEngine
 from pymnesia.query.query import Query
-from pymnesia.entities.registry import registry
+from pymnesia.entities.registry.interface import PymnesiaRegistryInterface
+
+__all__ = [
+    "QueryEngineType",
+    "query_engine_metaclass",
+]
 
 
 class QueryEngineType(type):
@@ -9,16 +15,26 @@ class QueryEngineType(type):
     Metaclass for QueryEngine.
     """
 
-    def __new__(mcs, name, bases, attrs):
+    _registry: PymnesiaRegistryInterface = None
+
+    def __new__(mcs, name, bases, attrs):  # pylint: disable=unused-argument
         def table_maker(entity_class_):
             def table_query_maker(self):
                 return Query(entity_class=entity_class_, unit_of_work=self.unit_of_work)
 
             return table_query_maker
 
-        for entity_cls_resolver in registry.all_configs():
+        for entity_cls_resolver in mcs._registry.all_configs():
             table = table_maker(entity_cls_resolver)
             table.__name__ = entity_cls_resolver.__tablename__
             attrs[table.__name__] = table
 
-        return super().__new__(mcs, name, bases, attrs)
+        return super().__new__(mcs, name, (QueryEngine, ), attrs)
+
+
+def query_engine_metaclass(registry_: PymnesiaRegistryInterface):
+    return type(
+        QueryEngineType.__name__,
+        (QueryEngineType, ),
+        {"_registry": registry_}
+    )
