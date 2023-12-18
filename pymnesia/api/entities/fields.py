00736e3d3acb91c3e350823f248689cc0ea06576
Harold Cohen <87488839+haroldcohen@users.noreply.github.com>
2023-12-18 15:34:06 +0100

    refactor(entities): move relation api to fields (#38)

--- pymnesia/api/entities/fields.py
+++ pymnesia/api/entities/fields.py
@@ -3,6 +3,7 @@
 from typing import Any, Callable
 
 from pymnesia.core.entities.field import Field, UNDEFINED
+from pymnesia.core.entities.relations import Relation
 
 
 def field(
@@ -10,6 +11,7 @@ def field(
         default_factory: Callable = UNDEFINED,
 ):
     """Returns a Field instance.
+    The instance is to be processed later on by the entity metaclass and various procedural functions.
 
     :param default: The default value for the field.
     :param default_factory: A callback used to return a default value for the field.
@@ -18,3 +20,20 @@ def field(
     if default is not UNDEFINED:
         return Field(default=default)
     return Field(default_factory=default_factory)
+
+
+def relation(
+        reverse: str,
+        is_nullable: bool = True,
+) -> Relation:
+    """Returns a Relation instance.
+    The instance is to be processed later on by the entity metaclass and various procedural functions.
+
+    :param reverse: The name of the reverse relation.
+    :param is_nullable: Whether the relation is nullable or not.
+    :return: A Relation instance.
+    """
+    return Relation(
+        reverse=reverse,
+        is_nullable=is_nullable,
+    )
