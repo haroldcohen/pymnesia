f83ebcadcf11a5c3fd306a0debc80da8ded4ddf4
Harold Cohen <87488839+haroldcohen@users.noreply.github.com>
2023-12-18 14:53:24 +0100

    refactor(query): use functools partial instead of runner (#36)

--- pymnesia/core/composition/__init__.py
+++ pymnesia/core/composition/__init__.py
@@ -1,36 +1,6 @@
 from functools import reduce
 
 
-def curry(func):
-    """Provides with curry like functions"""
-    f_args, f_kwargs = [], {}  # pylint: disable=unused-variable
-
-    def f(*args, **kwargs):  # pylint: disable=invalid-name
-        nonlocal f_args, f_kwargs
-        if args or kwargs:
-            f_args += args
-            f_kwargs.update(kwargs)
-            return f
-        result = func(*f_args, **f_kwargs)
-        f_args, f_kwargs = [], {}
-        return result
-
-    return f
-
-
-def runner(func, *args, **kwargs):
-    """Simple composition function"""
-
-    def f(*f_args, **f_kwargs):  # pylint: disable=invalid-name
-        f_kwargs.update(kwargs)
-
-        return func(*f_args + args, **f_kwargs)
-
-    f.__name__ = func.__name__
-
-    return f
-
-
 def composite(*func):
     """Functional composition"""
 
