26d524fbe76e1c45ea589910aa87dd7595ed3d56
Harold Cohen <87488839+haroldcohen@users.noreply.github.com>
2023-12-18 16:02:51 +0100

    feat(unit of work): add uow api (#40)

---
+++ pymnesia/api/unit_of_work/__init__.py
@@ -0,0 +1,19 @@
+"""Provides with unit of work related APIs.
+"""
+import time
+
+from pymnesia.core.unit_of_work.unit_of_work import UnitOfWork
+
+__all__ = [
+    "uow"
+]
+
+
+def uow():
+    """Returns an instance of UnitOfWork.
+    A unit of work is the equivalent of a database.
+    It stores entities and manages their states and related operations, such as save.
+
+    :return: An instance of UnitOfWork.
+    """
+    return UnitOfWork(state=time.time_ns())
